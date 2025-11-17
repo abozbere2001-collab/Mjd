6fbbd529c3452f4b9493eaf86e8ac8a9649b1050
عبد السلام <abozbere2001@gmail.com>
2025-11-17 09:40:44 +0000

    نفس المشاكل السابقه لا بي دبليو اي بولدر يتعرف ولا التطبيق عند تثبيته يف

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 
 
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // Add assetPrefix for GitHub Pages
-  assetPrefix: isGithubActions ? '/Mjd/' : undefined,
   typescript: {
     ignoreBuildErrors: true,
   },
